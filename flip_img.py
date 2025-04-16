7075d6496729aa6e1ac0c18dda4bcaa1721bfe13
Antoine Weisrock <antoine.weisrock@gmail.com>
2025-04-16 16:11:57 +0200

    style: cast Path to str to match signature

--- flip_img.py
+++ flip_img.py
@@ -11,7 +11,7 @@ def flip_img(img_pth: Path) -> None:
   """"""
   
   if img_pth.name.endswith("2.png") or img_pth.name.endswith("3.png"):
-    cv2.imwrite(img_pth, cv2.flip(cv2.imread(img_pth), -1))
+    cv2.imwrite(str(img_pth), cv2.flip(cv2.imread(str(img_pth)), -1))
 
 
 if __name__ == "__main__":
