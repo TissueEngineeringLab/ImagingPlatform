23b426e967fa3d9dd9a2da0c9fc904b9785b9b26
Antoine Weisrock <antoine.weisrock@gmail.com>
2025-05-22 18:19:07 +0200

    feat: preserve sign when computing effort

--- processing/post_process.py
+++ processing/post_process.py
@@ -131,9 +131,11 @@ def plot_results(img_calib_file: Path,
   # From the computed distance, calculate the effort for each well and moment
   force_dict = deepcopy(dist_dict)
   for quad, well in product((0, 1, 2, 3, 4, 5), (0, 1)):
+    sign = np.sign(force_dict[quad][well][1])
     force_dict[quad][well][1] = force_interp(
       abs(dist_dict[quad][well][1]),
       label_to_length[pos_to_label[(quad, well)]])
+    force_dict[quad][well][1] *= sign
 
   # Also retrieve the moments when the medium was refreshed
   refresh_timestamps = tuple(map(
