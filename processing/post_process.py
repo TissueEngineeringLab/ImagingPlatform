fix: incorrect calculation of force from displacement

processing/post_process.py
@@ -172,8 +172,8 @@ def plot_results(img_calib_file: Path,
   force_dict = deepcopy(dist_dict)
   for quad, well in product((0, 1, 2, 3, 4, 5), (0, 1)):
     sign = - np.sign(force_dict[quad][well][1])
-    force_dict[quad][well][1] = force_interp(
-      abs(dist_dict[quad][well][1]),
+    force_dict[quad][well][1] = 2 * force_interp(
+      abs(dist_dict[quad][well][1]) / 2,
       label_to_length[pos_to_label[(quad, well)]])
     force_dict[quad][well][1] *= sign
 